57f505f29ef4ea62387418f26878288d22461cc7
TaraWilson17 <wwtara@hotmail.com>
2018-10-24 19:41:58 -0700

    Adding practice with Bayes Theorem and probability

--- probability.R
+++ probability.R
@@ -1,8 +1,21 @@
-install.packages('ggplot2')
+install.packages('LaplacesDemon', 'C:/Users/wwtar/OneDrive/Documents/R/win-library/3.5')
 
-#install.packages('LaplacesDemon')
+library(LaplacesDemon)
 
-#  Bayes Theorem Practice
+# Bayes Theorem:
+# Pr(A|B) = Pr(B|A)Pr(A) / Pr(B)
 PrA <- c(0.75,0.25)
 PrBA <- c(6/9, 5/7)
 BayesTheorem(PrA, PrBA)
+
+# to get upper and lower limits of 95% confidence interval
+qnorm(0.025)
+qnorm(1-0.025)
+
+# confidence limit in single value for 95% confidence interval
+cl<-0.95
+qnorm((1+cl)/2)
+
+# binomial formula
+binom.test(x=39, n=215, p=0.15)
+
